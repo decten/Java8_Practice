c658736916334e8ecd1e75c94b0777dfefd822a2
hyun <vikim1210@naver.com>
2023-10-09 02:05:00 +0900

    FunctionMain: Supplier의 람다&함수 구현 활용

--- src/main/java/org/example/function/FunctionMain.java
+++ src/main/java/org/example/function/FunctionMain.java
@@ -5,6 +5,20 @@ import java.util.function.Supplier;
 public class FunctionMain {
 
     public static void main(String[] args) {
-        Supplier<String> myStringSupplier;
+        Supplier<String> myStringSupplier = () -> "hello world!";
+        System.out.println(myStringSupplier.get());
+
+        Supplier<Double> myRandomDoubleSupplier = () -> Math.random();
+        System.out.println(myRandomDoubleSupplier.get());
+        System.out.println(myRandomDoubleSupplier.get());
+
+        printRandomDoubles(myRandomDoubleSupplier, 2);
+
+    }
+    public static void printRandomDoubles(Supplier<Double> randomSupplier,
+        int count){
+        for (int i = 0; i < count; i++) {
+            System.out.println(randomSupplier.get());
+        }
     }
 }
