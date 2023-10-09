0ee562ca0af80e97bbcba467a2c49f3159b2194c
hyun <vikim1210@naver.com>
2023-10-09 23:16:17 +0900

    Consumer 인터페이스 사용 & FunctionMain에 Consumer 사용

--- src/main/java/org/example/function/FunctionMain.java
+++ src/main/java/org/example/function/FunctionMain.java
@@ -1,11 +1,21 @@
 package org.example.function;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import org.example.function.util.Consumer;
 
 public class FunctionMain {
 
+    public static void printRandomDoubles(Supplier<Double> randomSupplier,
+        int count){
+        for (int i = 0; i < count; i++) {
+            System.out.println(randomSupplier.get());
+        }
+    }
     public static void main(String[] args) {
-        Supplier<String> myStringSupplier = () -> "hello world!";
+        /* Supplier */
+        Supplier<String> myStringSupplier = () -> "hello supplier";
         System.out.println(myStringSupplier.get());
 
         Supplier<Double> myRandomDoubleSupplier = () -> Math.random();
@@ -14,11 +24,19 @@ public class FunctionMain {
 
         printRandomDoubles(myRandomDoubleSupplier, 2);
 
+        /* Consumer */
+        Consumer<String> myStringConsumer = (String str) -> System.out.println(str);
+        myStringConsumer.accept("hello consumer");
+
+        List<Integer> integerInputs = Arrays.asList(4,2,3);
+        Consumer<Integer> myIntegerConsumer = (Integer x) ->
+            System.out.println("Processing Integer: "+x);
+        process(integerInputs,myIntegerConsumer);
     }
-    public static void printRandomDoubles(Supplier<Double> randomSupplier,
-        int count){
-        for (int i = 0; i < count; i++) {
-            System.out.println(randomSupplier.get());
+
+    public static void process(List<Integer> inputs, Consumer<Integer> processor){
+        for(Integer input: inputs){
+            processor.accept(input);
         }
     }
 }
